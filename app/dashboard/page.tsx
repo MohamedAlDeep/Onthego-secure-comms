32c1544bc266cabe2229f4795beb5d4a6925a759
MohamedAlDeep <mohamedsamir22446688@gmail.com>
2025-08-02 18:32:18 +0300

    Fixed db

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,15 @@
 import { useLocalStorage } from "usehooks-ts"
-// import {cookies} from 'next/headers'
+// import { useCookies } from "next-client-cookies"
+import {cookies} from 'next/headers'
+import { redirect, RedirectType } from "next/navigation";
 
 export default async function Dashboard() {
-    // const cookieStore = await cookies()
-    // const username = cookieStore.get('username')
-    // const uid = cookieStore.get('uid')
-
+    const cookieStore = await cookies()
+    const username = cookieStore.get('username')
+    const uid = cookieStore.get('uid')
+    if(!username || !uid) {
+        redirect("/", RedirectType.replace)
+    }
 
     return(
         <div>
--- app/lib/openpgp.tsx
+++ app/lib/openpgp.tsx
@@ -1,35 +1,35 @@
-import { encrypt, 
-    decrypt, 
-    generateKey, 
-    createMessage, 
-    readKey } from 'openpgp'
+// import { encrypt, 
+//     decrypt, 
+//     generateKey, 
+//     createMessage, 
+//     readKey } from 'openpgp'
 
-export async function generateKeys(username, passphrase) {
-    const { privateKey, publicKey, revocationCertificate  } = await generateKey({
-        type: 'ecc',
-        curve: "ed25519",
-        userIDs: [{ name: username }],
-        passphrase: passphrase,
-        format: 'armored'
-    });
-    console.log('Private Key:', privateKey);
-    console.log('Public Key:', publicKey);
-    console.log('Revocation Certificate:', revocationCertificate);
-    return { privateKey, publicKey, revocationCertificate };
-}
+// export async function generateKeys(username, passphrase) {
+//     const { privateKey, publicKey, revocationCertificate  } = await generateKey({
+//         type: 'ecc',
+//         curve: "ed25519",
+//         userIDs: [{ name: username }],
+//         passphrase: passphrase,
+//         format: 'armored'
+//     });
+//     console.log('Private Key:', privateKey);
+//     console.log('Public Key:', publicKey);
+//     console.log('Revocation Certificate:', revocationCertificate);
+//     return { privateKey, publicKey, revocationCertificate };
+// }
 
-export async function encryptMessage(publicKey:any, message: any) {
-    const encrypted = await encrypt({
-        message: await createMessage({ text: message }),
-        encryptionKeys: await readKey({ armoredKey: publicKey })
-    });
-    return encrypted;
-}
+// export async function encryptMessage(publicKey:any, message: any) {
+//     const encrypted = await encrypt({
+//         message: await createMessage({ text: message }),
+//         encryptionKeys: await readKey({ armoredKey: publicKey })
+//     });
+//     return encrypted;
+// }
 
-export async function decryptMessage(privateKey:any, passphrase: string, encryptedMessage: string) {
-    const decrypted = await decrypt({
-        message: await createMessage({ text: encryptedMessage }),
-        decryptionKeys: await readKey({ armoredKey: privateKey, passphrase })
-    });
-    return decrypted.data;
-}
+// export async function decryptMessage(privateKey:any, passphrase: string, encryptedMessage: string) {
+//     const decrypted = await decrypt({
+//         message: await createMessage({ text: encryptedMessage }),
+//         decryptionKeys: await readKey({ armoredKey: privateKey, passphrase })
+//     });
+//     return decrypted.data;
+// }
