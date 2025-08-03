dac3cbe1eaa48d1a5783e4fceff47a859e1be1c1
MohamedAlDeep <mohamedsamir22446688@gmail.com>
2025-08-03 17:31:47 +0300

    New routes & Design - Generate keys route - Encrypt - Decrypt

---
+++ app/decrypt/page.tsx
@@ -0,0 +1,68 @@
+
+import {cookies} from 'next/headers'
+import { redirect, RedirectType } from "next/navigation";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import Navbar from "@/components/NavBar/Navbar";
+import Footer from "@/components/Footer/Footer";
+import { Car } from 'lucide-react';
+import { Button } from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
+
+export default async function Encrypt() {
+    const cookieStore = await cookies()
+    const username = cookieStore.get('username')
+    const uid = cookieStore.get('uid')
+    if(!username || !uid) {
+        redirect("/", RedirectType.replace)
+    }
+
+    return(
+         <div className="min-h-screen bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900 p-4">
+
+            <Navbar />
+                <div className="flex justify-center w-full my-8">
+                    <Card className="w-full max-w-xl bg-slate-800/90 border-purple-500/20 backdrop-blur-sm border-2" style={{ borderColor: '#9810FA' }}>
+                        <div className="flex justify-center items-center">
+                            <h1 className="text-white text-3xl">Decrypt</h1>
+                        </div>
+                    </Card>
+                </div>
+
+               <div className="flex flex-row justify-center items-start w-full gap-4">
+                    <Card className="bg-slate-800/90 w-full max-w-3xl border-purple-500/20 backdrop-blur-sm border-2 text-white p-5 m-5" style={{ borderColor: '#9810FA' }}>
+                        <h1 className="flex justify-center items-center text-2xl font-bold mb-4">Cypher</h1>
+                        <Card className="h-45 bg-purple-500/20  border-purple-500/20 backdrop-blur-sm border-2 text-white p-5 m-5" style={{ borderColor: '#9810FA' }}>
+                                <div className="flex items-center justify-between ">
+                                <textarea className='h-40 w-140 m-0 border-0 ' name="" id="" style={{ resize: 'none' }} placeholder='Enter message content'></textarea>
+                                </div>
+                        </Card>
+                        <div className="flex justify-end">
+                            <Button className="w-40 bg-gradient-to-r from-purple-600 to-pink-600 hover:from-purple-700 hover:to-pink-700 m-5">
+                                Decrypt
+                            </Button>
+                        </div>
+                    </Card>
+                    <Card className="bg-slate-800/90 w-full max-w-3xl border-purple-500/20 backdrop-blur-sm border-2 text-white p-5 m-5" style={{ borderColor: '#9810FA' }}>
+                        <h1 className="flex justify-center items-center text-2xl font-bold mb-4">Message</h1>
+                        <div>
+                            <Card className="h-45 bg-purple-500/20  border-purple-500/20 backdrop-blur-sm border-2 text-white p-5 m-5" style={{ borderColor: '#9810FA' }}>
+                                <div className="flex items-center justify-between ">
+                                <textarea className='h-40 w-140 m-0 border-0 ' name="" id="" style={{ resize: 'none' }}  disabled></textarea>
+                                </div>
+                        </Card>
+                                <div className="flex justify-end">
+                                    <Button className="w-40 bg-gradient-to-r from-purple-600 to-pink-600 hover:from-purple-700 hover:to-pink-700 m-5">
+                                        Copy
+                                    </Button>
+                                </div>
+                        </div>
+                    </Card>
+                </div>
+
+                
+
+            <Footer />
+        </div>
+    )
+
+}
--- components/NavBar/Navbar.tsx
+++ components/NavBar/Navbar.tsx
@@ -7,6 +7,7 @@ export default async function Navbar(){
         
         <Card className="w-full w-150  bg-slate-800/90 border-purple-500/20 backdrop-blur-sm border-2 " style={{ borderColor: '#9810FA' }}>
         <div className="flex justify-between items-center px-4 m-1 text-white w-130">    
+            <a href="/dashboard" className="rounded-[11px] px-3 mx-5 transition-all duration-300 ease-in-out hover:bg-[#BD2ABA]" style={{ borderTop: '2px solid transparent', borderLeft: '2px solid transparent', borderRight: '2px solid transparent', borderBottom: '2px solid #BD2ABA' }}>Dashboard</a>
             <a href="/keys" className="rounded-[11px] px-3 mx-5 transition-all duration-300 ease-in-out hover:bg-[#BD2ABA]" style={{ borderTop: '2px solid transparent', borderLeft: '2px solid transparent', borderRight: '2px solid transparent', borderBottom: '2px solid #BD2ABA' }}>Keys</a>
             <a href='encrypt' className="rounded-[11px] px-3 transition-all duration-300 ease-in-out hover:bg-[#BD2ABA]" style={{ borderTop: '2px solid transparent', borderLeft: '2px solid transparent', borderRight: '2px solid transparent', borderBottom: '2px solid #BD2ABA' }}>Encryption</a>
             <a href='decrypt' className="rounded-[11px] px-3 transition-all duration-300 ease-in-out hover:bg-[#BD2ABA]" style={{ borderTop: '2px solid transparent', borderLeft: '2px solid transparent', borderRight: '2px solid transparent', borderBottom: '2px solid #BD2ABA' }}>Decryption</a>
