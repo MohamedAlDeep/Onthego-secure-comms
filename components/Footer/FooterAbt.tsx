4988e7cfc3ab518b73795d1937d398225eeaa593
MohamedAlDeep <mohamedsamir22446688@gmail.com>
2025-08-06 10:14:00 +0300

    login button

--- components/Footer/FooterAbt.tsx
+++ components/Footer/FooterAbt.tsx
@@ -6,7 +6,8 @@ export default function FooterAbt(){
         
 
         <div className="flex w-full h-20 justify-between bg-slate-800/90  backdrop-blur-sm  items-center px-4 m-1 text-white w-130">    
-            <div className="">    
+            <div className="">   
+                <a href="/"  className="rounded-[11px] px-5 py-1.5 mx-5 transition-all duration-300 ease-in-out hover:bg-[#BD2ABA]" >Login</a> 
                 <a href="/about" className="rounded-[11px] px-5 py-1.5 mx-5 transition-all duration-300 ease-in-out hover:bg-[#BD2ABA]" >About</a>
                 <a href='' className="rounded-[11px] px-5 py-1.5 mx-5 transition-all duration-300 ease-in-out hover:bg-[#BD2ABA]" >Feedback</a>
                 <a href='https://github.com/MohamedAlDeep/Onthego-secure-comms' className="rounded-[11px] px-5 py-1.5 mx-5 transition-all duration-300 ease-in-out hover:bg-[#BD2ABA]" >Contribute</a>
